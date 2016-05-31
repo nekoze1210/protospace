4d41061670437c0151198ea6404ee1fa55f0906a
Daiki Nagaoka <Daiki@lneck.local>
2016-05-31 11:15:33 +0900

    added only options on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
   resources :prototypes
-  resources :users
+  resources :users, only: [:index, :show, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
