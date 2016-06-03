c2e589f97fe28b61aaf35391c4f0fb895e3d92b7
Daiki Nagaoka <Daiki@lneck.local>
2016-06-04 01:02:27 +0900

    changed after_sign_out_path  root_path -> new_user_session_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def after_sign_out_path_for(resource)
-    root_path
+    new_user_session_path
   end
 
   def configure_permitted_parameters
