501b1ff2f2be92f883af9ac4ec63eeca9ae3f31c
Daiki Nagaoka <Daiki@lneck.local>
2016-06-07 19:06:15 +0900

    changed schema file automatically

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606081536) do
+ActiveRecord::Schema.define(version: 20160606092910) do
 
   create_table "images", force: :cascade do |t|
-    t.text     "thumbnail",    limit: 65535
-    t.integer  "prototype_id", limit: 4
     t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.text     "thumbnail",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
