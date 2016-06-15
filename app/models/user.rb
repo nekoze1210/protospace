3cd1a8898160103a54f3130f9cb2d1bd15c1f7bd
Daiki Nagaoka <Daiki@lneck.local>
2016-06-15 14:59:18 +0900

    added user_image_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, UserimageUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :nickname, presence: true
-
   has_many :prototypes
 
 end
