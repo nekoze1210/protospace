d35c26e32a15de912347f7d8cd77346bf0518884
Daiki Nagaoka <Daiki@lneck.local>
2016-06-17 22:30:44 +0900

    added editing & deleting function in prototype_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_proto, only: [:show, :edit, :destroy]
+  before_action :set_proto, except: [:index, :new, :create]
 
   def index
     @protos = Prototype.includes(:user).order('created_at DESC').page(params[:page]).per(12)
@@ -25,12 +25,20 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @subs = @proto.protoimages.sub
   end
 
   def update
+    if @proto.update(proto_params)
+      redirect_to prototype_path(@proto), notice: 'Your prototype is updated successfully'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @proto.destroy
+    redirect_to root_path, notice: 'Your prototype is deleted'
   end
 
   private
@@ -39,10 +47,10 @@ class PrototypesController < ApplicationController
     end
 
     def proto_params
-      params.require(:prototype).permit(
+      params.require(:prototype).permit( \
         :title,
         :catch_copy,
         :concept,
-        protoimages_attributes:[:thumbnail, :role]).merge(user_id: current_user.id)
+        protoimages_attributes:[ :id, :thumbnail, :role, :prototype_id, :_destroy]).merge(user_id: current_user.id)
     end
 end
