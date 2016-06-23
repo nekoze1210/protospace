32aab00e00f441bf71d2f50f8921db5333f0a55e
Daiki Nagaoka <Daiki@lneck.local>
2016-06-23 17:01:05 +0900

    added controller 'comments_controller'

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  before_action :set_proto
+
+  def create
+    @proto.comments.create(comment_params)
+    @comments = @proto.comments
+  end
+
+  private
+  def set_proto
+    @proto = Prototype.find(params[:prototype_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:id, :text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
