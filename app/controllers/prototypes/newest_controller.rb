bc29a8fcad2b9585f0ca70b3d4dd55af819debdd
Daiki Nagaoka <Daiki@lneck.local>
2016-06-26 01:15:22 +0900

    fix indents

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::NewestController < ApplicationController
 
   def index
-@protos = Prototype.includes(:user).order('created_at DESC').page(params[:page]).per(12)
+    @protos = Prototype.includes(:user).order('created_at DESC').page(params[:page]).per(12)
   end
 
 end
