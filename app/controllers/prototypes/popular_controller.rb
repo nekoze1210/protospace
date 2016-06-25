69c3734f1549c025f16a53a7126794ba8b91c3d5
Daiki Nagaoka <Daiki@lneck.local>
2016-06-25 18:27:59 +0900

    added popular controller, and its view

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,6 +1,7 @@
-class PopularController < ApplicationController
+class Prototypes::PopularController < ApplicationController
 
   def index
+    @protos = Prototype.eager_load(:user).order("likes_count DESC").page(params[:page]).per(12)
   end
 
 end
