7612eb800b7bbfc9ea393cc812886cb415f28762
Daiki Nagaoka <Daiki@lneck.local>
2016-06-28 16:33:20 +0900

    Fix N+1 Problem

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,12 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = ActsAsTaggableOn::Tag.most_used
+    @tags = ActsAsTaggableOn::Tag.most_used.includes(:taggings)
   end
 
   def show
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
-    @protos = Prototype.tagged_with(@tag).order('created_at DESC').page(params[:page]).per(12)
+    @protos = Prototype.tagged_with(@tag).includes(:user).order('created_at DESC').page(params[:page]).per(12)
   end
 
 end
