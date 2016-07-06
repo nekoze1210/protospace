db9c54f4ef7f9f817be26ca77e77e82da3f95dba
Daiki Nagaoka <Daiki@lneck.local>
2016-07-06 11:57:19 +0900

    Create User_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "is invalid without a nickname" do
+    end
+  end
+
+  describe '#update' do
+    it 'is invalid without a nickname' do
+    end
+  end
+
+end
