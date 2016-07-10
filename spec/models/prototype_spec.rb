207aeca68b84d52bd5b432ff40b4f69b38e236a1
Daiki Nagaoka <Daiki@lneck.local>
2016-07-10 09:55:02 +0900

    Create prototype_spec.rb

---
+++ spec/models/prototype_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+describe Prototype do
+  describe 'associations' do
+
+    describe 'with user' do
+      it 'is associated with an user' do
+        user = create(:user)
+        proto = create(:prototype, user: user)
+        expect(proto.user).to eq user
+      end
+    end
+
+    describe 'with comments' do
+      it 'deletes the comments when Prototype is deleted' do
+        proto = create(:prototype)
+        comment = create(:comment, prototype: proto)
+        expect { proto.destroy }.to change{ Comment.count }.by(-1)
+      end
+    end
+
+    describe 'with likes' do
+      it 'deletes the likes when Prototype is deleted' do
+        proto = create(:prototype)
+        like = create(:like, prototype: proto)
+        expect { proto.destroy }.to change { Like.count }.by(-1)
+      end
+    end
+  end
+
+  describe '#create' do
+    it 'is invalid without a title' do
+      proto = build(:prototype, title: nil)
+      proto.valid?
+      expect(proto.errors[:title]).to include ("can't be blank")
+    end
+  end
+
+  describe 'liked_by?(user)' do
+    describe 'when liked_by an user' do
+      it 'returns true' do
+        user = create(:user)
+        like = create(:like, user: user)
+        liked_by = Like.find_by(user_id: user.id)
+        expect(liked_by).to be_truthy
+      end
+    end
+
+    describe 'when not liked_by an user' do
+      it 'returns nil' do
+        user = create(:user)
+        like = create(:like, user: nil)
+        liked_by = Like.find_by(user_id: user.id)
+        expect(liked_by).to be_nil
+      end
+    end
+  end
+
+  describe '#set_sub_thumbnails' do
+      let(:proto){create(:prototype)}
+      let!(:sub_imgs) {
+        [
+          create(:protoimage, prototype: proto, role: :sub),
+          create(:protoimage, prototype: proto, role: :sub),
+          create(:protoimage, prototype: proto, role: :sub)
+        ]
+      }
+      it 'returns an array containing sub_images' do
+      expect(sub_imgs).to include(sub_imgs[0], sub_imgs[1], sub_imgs[2])
+    end
+  end
+end
