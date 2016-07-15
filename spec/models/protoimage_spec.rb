18788883f4a439c85172c1823a2f57367ea93905
Daiki Nagaoka <Daiki@lneck.local>
2016-07-16 00:00:22 +0900

    Add let to the variable thumb

--- spec/models/protoimage_spec.rb
+++ spec/models/protoimage_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
 describe 'Protoimage' do
-  describe '#create' do
+  describe 'validation' do
+    let(:image) { attributes_for(:protoimage) }
     it 'has a valid factory' do
-      thumb = build(:protoimage, :main)
-      expect(thumb).to be_valid
+      expect(image).to be_present
     end
+  end
 
+  context '#create' do
+    let!(:thumb) { build(:protoimage, thumbnail: nil) }
     it 'is invalid without a thumbnail' do
-      thumb = build(:protoimage, thumbnail: nil)
       thumb.valid?
-      expect(thumb.errors[:thumbnail]).to include ("can't be blank")
+      expect(thumb.errors[:thumbnail][0]).to include ("can't be blank")
     end
 
     it 'has the wrong content format' do
-      thumb = build(:protoimage, thumbnail: fixture_file_upload("img/sample.rb", 'image/rb') )
+      thumb.thumbnail = fixture_file_upload("img/sample.rb", 'image/rb')
       thumb.valid?
       expect(thumb.errors[:thumbnail][0]).to include ("You are not allowed to upload \"rb\" files, allowed types: jpg, jpeg, gif, png")
     end
