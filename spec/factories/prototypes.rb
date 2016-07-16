4ecee797af1eaa73c3ea370e31b2682b76b37512
Daiki Nagaoka <Daiki@lneck.local>
2016-07-17 02:25:21 +0900

    Add Faker of created_at  in prototypes.rb

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     title               { Faker::Commerce.product_name }
     catch_copy          { Faker::Company.catch_phrase }
     concept             { Faker::Lorem.sentence }
+    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 
 end
