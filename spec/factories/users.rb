803e0681a0aa529d00d23af11417359ec24ff443
Daiki Nagaoka <Daiki@lneck.local>
2016-07-20 21:43:34 +0900

    Revert "Modify the password in users factory because of warning from devise"
    This reverts commit 376cff76a080bdb171597103582f968013865504.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :user do
+    password = Faker::Internet.password(8)
     nickname               { Faker::Name.name }
     email                  { Faker::Internet.email }
     profile                { Faker::Lorem.sentence }
     member                 { Faker::Company.name  }
     work                   { Faker::App.name }
-    password               '12345678'
-    password_confirmation  '12345678'
+    password               password
+    password_confirmation  password
   end
 end
