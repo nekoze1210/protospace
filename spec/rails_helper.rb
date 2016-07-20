45d6b1301a7a30ed11872c289f4664c34bd957e7
Daiki Nagaoka <Daiki@lneck.local>
2016-07-20 21:41:48 +0900

    Add config of Capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
+require 'capybara/poltergeist'
+require 'selenium-webdriver'
+Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 5
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,11 +36,13 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  Capybara.default_driver = :selenium
   config.include FactoryGirl::Syntax::Methods
+  config.use_transactional_fixtures = false
    # for Devise
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
+  config.include Capybara::DSL
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
