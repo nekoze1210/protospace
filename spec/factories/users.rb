376cff76a080bdb171597103582f968013865504
Daiki Nagaoka <Daiki@lneck.local>
2016-07-20 21:42:35 +0900

    Modify the password in users factory because of warning from devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    password = Faker::Internet.password(8)
     nickname               { Faker::Name.name }
     email                  { Faker::Internet.email }
     profile                { Faker::Lorem.sentence }
     member                 { Faker::Company.name  }
     work                   { Faker::App.name }
-    password               password
-    password_confirmation  password
+    password               '12345678'
+    password_confirmation  '12345678'
   end
 end
